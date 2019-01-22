d3a389ea30d71e1f113d4b7bf7679889e6dd90a2
pylover <vahid.mardani@gmail.com>
2019-01-22 23:13:04 +0330

    Initial setup.py

---
+++ easycli/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = '0.1.0'
+
+
+from .argument import Argument
+from .command import Command, SubCommand
+from .root import Root
+
---
+++ setup.py
@@ -0,0 +1,39 @@
+import re
+import os.path
+from setuptools import setup, find_packages
+
+# reading package's version (same way sqlalchemy does)
+with open(
+    os.path.join(os.path.dirname(__file__), 'easycli', '__init__.py')
+) as v_file:
+    package_version = \
+        re.compile('.*__version__ = \'(.*?)\'', re.S)\
+        .match(v_file.read())\
+        .group(1)
+
+
+setup(
+    name='easycli',
+    version=package_version,
+    author='Vahid Mardani',
+    author_email='vahid.mardani@gmail.com',
+    url='http://github.com/pylover/easycli',
+    description='Easily define your Command line and sub-commands using ' \
+        'argparse.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important!
+    install_requires=dependencies,
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 1 - Planning',
+        #'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        ]
+    )

2b5879c082d5aadfa987f625c035f4b1f6831111
pylover <vahid.mardani@gmail.com>
2019-03-01 08:49:22 +0330

    Increating coveration

--- easycli/tests/test_bashcompletion.py
+++ easycli/tests/test_bashcompletion.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+from os import path
+
 from easycli import Root, SubCommand, Argument
 
 from bddcli import stdout, status, stderr, Application, Command, when
@@ -27,9 +31,9 @@ Sub commands:
 '''
 
 
-def test_subcommand():
+def test_bash_autocompletion_systemwide():
     app = Application('foo', 'easycli.tests.test_bashcompletion:main')
-    with Command(app, 'Subcommand application without any argument'):
+    with Command(app, 'Test bash autocompletion'):
         assert stdout == EXPECTED_HELP
         assert status == 0
 
@@ -39,4 +43,26 @@ def test_subcommand():
         assert stdout == EXPECTED_HELP
 
         when('Install completion', arguments=['completion', 'install'])
-        when('Install completion', arguments=['completion', 'uninstall'])
+        when('Un.nstall completion', arguments=['completion', 'uninstall'])
+
+
+def test_bash_autocompletion_virtualenv():
+    app = Application('foo', 'easycli.tests.test_bashcompletion:main')
+    with tempfile.TemporaryDirectory() as venvdir:
+        os.mkdir(path.join(venvdir, 'bin'))
+        with Command(
+            app,
+            'Bash autocompletion inside virtual environment',
+            environ={'VIRTUAL_ENV': venvdir}
+        ):
+            assert stdout == EXPECTED_HELP
+            assert status == 0
+
+            when('help', arguments=['-h'])
+            assert status == 0
+            assert stderr == ''
+            assert stdout == EXPECTED_HELP
+
+            when('Install completion', arguments=['completion', 'install'])
+            when('Uninstall completion', arguments=['completion', 'uninstall'])
+

2673d82db012789200b520e0f3d6535588359a94
pylover <vahid.mardani@gmail.com>
2020-11-27 05:04:28 +0330

    v1.7.0

--- easycli/__init__.py
+++ easycli/__init__.py
@@ -1,8 +1,8 @@
-
+"""Uncomplicated OOP command line argument parser."""
 from .argument import Argument
 from .command import Command, SubCommand
 from .root import Root
 from .progressbar import ProgressBar
 
 
-__version__ = '1.6.0'
+__version__ = '1.7.0'

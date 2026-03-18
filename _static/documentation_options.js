1619b9f14d47b1832a2b0867ce276477c1d18784
pylover <pylover@users.noreply.github.com>
2026-03-18 17:37:34 +0000

    Deploying to gh-pages from @ pylover/easycli@c4c020158b0c34db677449aa976c6554687075ea 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.9.3',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.9.4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
